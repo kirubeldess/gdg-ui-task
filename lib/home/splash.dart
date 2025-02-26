4566a8af812599fe0c179530604be2276a05e73f
Kirubel <kirubeldesalegn17@gmail.com>
2025-02-26 21:45:10 +0300

    splash

---
+++ lib/home/splash.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:ui_task/home/details.dart';
+
+class MySplash extends StatelessWidget {
+  const MySplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 11, 67, 113),
+                Colors.blue.withAlpha((0.5 * 255).toInt()),
+              ],
+              begin: Alignment.topCenter, 
+              end: Alignment.bottomCenter,
+              )
+          ),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/gdgtas.jpg',
+                    width: 280,
+                    height: 280,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Text(
+                  'Enjoy',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w900,
+                    height: 1.4,
+                  ),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  'Your Meal',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w900,
+                    height: 1.4,
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: ElevatedButton(onPressed: () {
+                    
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                const DetailsScreen(),
+                                )
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 56, vertical: 16),
+                    backgroundColor:  Colors.white,
+                    foregroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Get Started',
+                    style:TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20,
+                    )),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
